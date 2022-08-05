b22d69aa4baccccc2b6b7e059fb5a624d283c48b
AndrewLin95 <andrew.lin88@hotmail.com>
2022-08-05 15:19:36 -0400

    remove testing script

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Header from "./Components/Header";
 import Home from "./Components/Pages/Home";
 import Locnar from "./Components/Pages/Locnar";
-import Test from "./Components/Pages/Test";
 
 function App() {
   return (
     <BrowserRouter>
       <Header/>
-      <Test/>
         <Routes>
           <Route path="/wheres_waldo/" element={<Home/>}/>
           <Route path="/wheres_waldo/locnar" element={<Locnar/>} />
